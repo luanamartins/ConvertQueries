0ad212c9818c8e6e6f7669dec9cf81bf321655f0
luanamartins <lms7@cin.ufpe.br>
2015-05-27 07:43:00 -0300

    Rename of DatabaseUtils to TokenDatabaseUtils

--- ConvertingQueries/src/data/managers/DatabaseUtils.java
+++
@@ -1,9 +0,0 @@
-package data.managers;
-
-public class DatabaseUtils {
-
-	public static String SQL_SERVER = "SQL Server";
-	public static String ORACLE = "Oracle";
-	public static String POSTGRESQL = "PostgreSQL";
-	
-}
---
+++ ConvertingQueries/src/data/managers/TokenDatabaseUtils.java
@@ -0,0 +1,9 @@
+package data.managers;
+
+public final class TokenDatabaseUtils {
+
+	public String SQL_SERVER = "SQL Server";
+	public String ORACLE = "Oracle";
+	public String POSTGRESQL = "PostgreSQL";
+	
+}
