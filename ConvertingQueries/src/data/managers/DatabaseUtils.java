7369dcc8ce9a2a6fca314132734ab49ee4d7778e
luanamartins <lms7@cin.ufpe.br>
2015-05-26 22:23:20 -0300

    Make strings of the database utils.
    You must add a new database string here, for each new database.

---
+++ ConvertingQueries/src/data/managers/DatabaseUtils.java
@@ -0,0 +1,9 @@
+package data.managers;
+
+public class DatabaseUtils {
+
+	public static String SQL_SERVER = "SQL Server";
+	public static String ORACLE = "Oracle";
+	public static String POSTGRESQL = "PostgreSQL";
+	
+}
