39f87c863d4ad8b861d93f66917e4c0158fb6705
luanamartins <lms7@cin.ufpe.br>
2015-05-29 18:10:06 -0300

    First test case

---
+++ ConvertingQueries/src/testcases/TestFailure.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFailure {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
