a110d6bd5a55672a43d19550795c02b6cbdfce68
luanamartins <lms7@cin.ufpe.br>
2015-05-30 19:54:18 -0300

    Add word representations for statements

---
+++ ConvertingQueries/src/data/managers/TokenStatement.java
@@ -0,0 +1,14 @@
+package data.managers;
+
+public class TokenStatement {
+
+	public final static String SELECT_STATEMENT = "SELECT";
+	public final static String DISTINCT_STATEMENT = "DISTINCT";
+	public final static String GROUP_BY_STATEMENT = "GROUP BY";
+	public final static String HAVING_STATEMENT = "HAVING";
+	public final static String INNER_JOIN_STATEMENT = "INNER JOIN";
+	public final static String LEFT_JOIN_STATEMENT = "LEFT JOIN";
+	public final static String WHERE_STATEMENT = "WHERE";
+	public final static String FROM_STATEMENT = "FROM";
+	
+}
