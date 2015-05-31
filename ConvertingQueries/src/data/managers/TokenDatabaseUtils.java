eb1b73212180be8e33b4393838e4fcc91f81c68e
luanamartins <lms7@cin.ufpe.br>
2015-05-30 21:22:21 -0300

    Hot fix tokendatabaseutils strings

--- ConvertingQueries/src/data/managers/TokenDatabaseUtils.java
+++ ConvertingQueries/src/data/managers/TokenDatabaseUtils.java
@@ -2,8 +2,8 @@ package data.managers;
 
 public final class TokenDatabaseUtils {
 
-	public String SQL_SERVER = "SQL Server";
-	public String ORACLE = "Oracle";
-	public String POSTGRESQL = "PostgreSQL";
+	public static String SQL_SERVER = "SQL Server";
+	public static String ORACLE = "Oracle";
+	public static String POSTGRESQL = "PostgreSQL";
 	
 }
