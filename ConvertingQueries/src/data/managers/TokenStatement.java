80347a5d11ed8cdc5b67364e1a4ef5b4d231d8fe
luanamartins <lms7@cin.ufpe.br>
2015-06-02 11:15:31 -0300

    Add logical and update statement in tokens

--- ConvertingQueries/src/data/managers/TokenStatement.java
+++ ConvertingQueries/src/data/managers/TokenStatement.java
@@ -2,14 +2,32 @@ package data.managers;
 
 public class TokenStatement {
 
+	// Basic tokens
 	public final static String SELECT_STATEMENT = "SELECT";
+	public final static String WHERE_STATEMENT = "WHERE";
+	public final static String FROM_STATEMENT = "FROM";
+
+	// Modifiers tokens
 	public final static String DISTINCT_STATEMENT = "DISTINCT";
 	public final static String GROUP_BY_STATEMENT = "GROUP BY";
+	public final static String ORDER_BY_STATEMENT = "ORDER BY";
 	public final static String HAVING_STATEMENT = "HAVING";
 	public final static String INNER_JOIN_STATEMENT = "INNER JOIN";
 	public final static String LEFT_JOIN_STATEMENT = "LEFT JOIN";
-	public final static String WHERE_STATEMENT = "WHERE";
-	public final static String FROM_STATEMENT = "FROM";
 	public final static String LIKE_STATEMENT = "LIKE";
+	
+	// Logical tokens
+	public final static String AND_STATEMENT = "AND";
+	public final static String OR_STATEMENT = "OR";
+	
+	// Set tokens
+	public final static String MINUS_STATEMENT = "MINUS";
+	public final static String UNION_STATEMENT = "UNION";
+	public final static String INTERSECT_STATEMENT = "INTERSECT";
+	public final static String EXCEPT_STATEMENT = "EXCEPT";
+	
+	// Update tokens
+	public final static String UPDATE_STATEMENT = "UPDATE";
+	public final static String SET_STATEMENT = "SET";
 	
 }
