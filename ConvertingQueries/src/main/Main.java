bb216e897b48382cb3493e743ccc809b5f8d4a03
luanamartins <lms7@cin.ufpe.br>
2015-06-01 23:05:11 -0300

    small fix

--- ConvertingQueries/src/main/Main.java
+++ ConvertingQueries/src/main/Main.java
@@ -23,11 +23,11 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			String query = TokenUtils.readFile("files/script1.sql");
-			StringTokenizer tokens = new StringTokenizer(query);
+			
 			String fromDatabase = "SQL Server";
 			String toDatabase = "Oracle";
 			TokenManager tokenManager = new TokenManager(fromDatabase, toDatabase);
-			tokenManager.parse(tokens);
+			tokenManager.parse(query);
 			System.out.println();
 		} catch (IOException e) {
 			e.printStackTrace();
