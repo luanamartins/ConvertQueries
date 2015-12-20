d01eec6978e1a16d1e46d3139365780f046ecea3
Luana <lms7@cin.ufpe.br>
2015-12-20 10:23:01 -0300

    Insert class for create succeed tests

---
+++ ConvertingQueries/src/testcases/TestSucceed.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSucceed
+{
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
