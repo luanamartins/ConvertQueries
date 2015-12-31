0c1d95c2fe452385c94a7f972f738fdebda17d78
Luana <lms7@cin.ufpe.br>
2015-12-30 21:12:19 -0300

    Add Mysql to the database constants

--- ConvertQueries/src/data/managers/DatabaseName.java
+++ ConvertQueries/src/data/managers/DatabaseName.java
@@ -5,5 +5,5 @@ public final class DatabaseName {
 	public final static String SQL_SERVER = "SQL Server";
 	public final static String ORACLE = "Oracle";
 	public final static String POSTGRESQL = "PostgreSQL";
-
+	public final static String MY_SQL = "MySQL";
 }
