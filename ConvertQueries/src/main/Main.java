92c5404a0a6f8942df8109b82ac7a1ef4d0498b7
Luana <lms7@cin.ufpe.br>
2015-12-30 21:12:59 -0300

    Remove unnecessary comment

--- ConvertQueries/src/main/Main.java
+++ ConvertQueries/src/main/Main.java
@@ -7,18 +7,6 @@ import data.managers.TokenUtils;
 
 public class Main {
 
-	/**
-	 * Oracle Queries syntax:
-	 * http://docs.oracle.com/cd/E17952_01/refman-5.0-en/select.html
-	 * http://docs.
-	 * oracle.com/cd/B19306_01/server.102/b14200/statements_10002.htm
-	 * 
-	 * SQL Server Queries syntax:
-	 * https://msdn.microsoft.com/en-us/library/ms189499.aspx
-	 * https://msdn.microsoft.com/en-us/library/ms187731.aspx
-	 * 
-	 * */
-
 	public static void main(String[] args) {
 		try {
 			String query = TokenUtils.readFile("files/script1.sql");
