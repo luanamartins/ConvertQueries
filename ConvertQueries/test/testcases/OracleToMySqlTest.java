8dead229dcf28fccb5ef460741fb84ac9fa9e4f6
Luana <lms7@cin.ufpe.br>
2016-01-16 10:29:27 -0300

    Create new source package for tests

---
+++ ConvertQueries/test/testcases/OracleToMySqlTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OracleToMySqlTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
